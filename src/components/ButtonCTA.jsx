83ffb81b2289485ac25085f9d7c71ada9acbd99a
mocanuandrei1 <127875795+mocanuandrei1@users.noreply.github.com>
2024-03-12 18:07:19 +0200

    v1.3

--- src/components/ButtonCTA.jsx
+++ src/components/ButtonCTA.jsx
@@ -4,7 +4,7 @@ const ButtonCTA = (props) => {
   return (
     <div className="text-center my-10 mx-5">
       <a href="#redirectButtonLanding">
-        <button className="lg:w-[50%] md:w-full sm:w-full px-4 py-2  sm:px-6 sm:py-3 bg-energy-blue text-white rounded-3xl font-['Jost-Bold'] text-xl md:text-2xl sm:text-2lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+        <button className="lg:w-[35%] md:w-full sm:w-full px-4 py-2  md:px-6 md:py-4 bg-energy-blue text-white rounded-3xl font-['Jost-Bold'] text-xl md:text-2xl sm:text-2lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
           {props.text}
         </button>
       </a>
