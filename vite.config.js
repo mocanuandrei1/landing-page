0f7c9b8ea86eb9f0ee5763c3af6cbeb3e521ed3f
mocanuandrei1 <127875795+mocanuandrei1@users.noreply.github.com>
2024-03-13 15:05:40 +0200

    added vite-compression2

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import dotenv from "dotenv";
+import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), compression()],
   base: "/landing-page/",
   define: {
     "process.env.VITE_EMAILJS_PUBLIC_KEY": JSON.stringify(
